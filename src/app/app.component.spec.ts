d1ac6f15f805106ab395f8a3e9151d536b6135bb
Joshua Decosta <joshdecosta@gmail.com>
2019-01-16 17:22:28 -0800

    changes to angular.json file for deploying on github

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngtable'`, () => {
+  it(`should have as title 'mbta-transit'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngtable');
+    expect(app.title).toEqual('mbta-transit');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngtable!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mbta-transit!');
   });
 });
